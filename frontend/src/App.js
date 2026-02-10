a93464483b9ffeca100bc0befdd01074c2673bf9
Sachin <sachinchauhan.work138@gmail.com>
2026-02-10 14:13:45 +0530

    Fix: Anchor Philosophy section image to right screen edge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <section className="Philosophy">
-        <div style={{ position: 'absolute', right: 0, top: 0, width: '50vw', height: '100%' }}>
-          <img src="path/to/image.jpg" alt="Philosophy Image" style={{ objectFit: 'cover', width: '100%', height: '100%' }} />
-        </div>
-        {/* Other content of the Philosophy section */}
-      </section>
-    </div>
-  );
-}
-
-export default App;
+<div style={{ position: 'relative' }}>  <PhilosophySection>    <div style={{ position: 'absolute', right: 0, top: 0, width: '50vw', height: '100%' }}>      <img src="your_image_source_here" alt="Philosophy" />    </div>    {/* Other contents of PhilosophySection */}  </PhilosophySection></div>
